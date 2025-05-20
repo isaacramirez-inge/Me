77d937a77798f7004335fee2a8ee266eb15e69e9
isaacramirez.inge <isaacramirez.inge@gmail.com>
2025-05-20 21:52:29 +0000

    Se agrego internaiconalizacion con i18n nativa de astro, y redireccion desde la raiz

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     base: '/Me/',
-    output: 'static'
+    output: 'static',
+    i18n: {
+        locales: ['en-us', 'es-es', 'es-sv'],
+        defaultLocale: 'en-us',
+        routing:{
+            prefixDefaultLocale: false,
+        }
+    }
 });
