e5e7c053e17903c2097cb44e1d7c6dc44d9c5760
isaacramirez.inge <isaacramirez.inge@gmail.com>
2025-06-13 22:01:43 +0000

    Primera version estable con presentacion, falta verificar imagen en produccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +13,11 @@ export default defineConfig({
         routing:{
             prefixDefaultLocale: false,
         }
-    }
+    },
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [
+        lottie(),
+    ]
 });
